bfc647d1c2c8ae4b589731709ee3d391ff2006c3
WD1121 <98111857+WD1121@users.noreply.github.com>
2022-02-23 21:32:10 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -71,6 +71,11 @@ TEMPLATES = [
     },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
@@ -93,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -126,3 +131,4 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+LOGIN_URL = 'rango:login'
